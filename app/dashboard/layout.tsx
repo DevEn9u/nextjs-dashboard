7422896df7156b75ac4d30190dd90cff3ff24746
DevEn <kangingoo31@gmail.com>
2024-05-23 17:49:20 +0900

    first commit

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+
+// layout의 children으로 page, 여기서는 invocies와 customers의 page가 들어옴
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import '@/app/ui/global.css';
+import { pretendard } from '@/app/ui/fonts';
+
+// 최상위 레이아웃, body 태그 역할, 루트 레이아웃이라고 칭함
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ko">
+      <body className={pretendard.className}>{children}</body>
     </html>
   );
 }
